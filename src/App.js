c555913026330a150c9f5fd7274c1c9133c484f1
Amy Vier <amyvier@icloud.com>
2024-02-25 21:19:34 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PetInfoBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { dynamicVariable: 500 };
+    }
+
+    decrease() {
+        if (this.state.dynamicVariable > 0) {
+            this.setState(state => ({
+                dynamicVariable: state.dynamicVariable - 5
+            }));
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.decrease(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+      let style = {
+        backgroundColor: "DodgerBlue",
+        height: "20px",
+        width: `${this.state.dynamicVariable}px`
+      };
+        return <div style={style}> </div>;
+    }
 }
 
-export default App;
+export default PetInfoBar;
